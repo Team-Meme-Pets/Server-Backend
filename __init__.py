75e53824ea6170cd00c905d52d7bcc90cb2cbc79
Iven Yang <iven_yang@yahoo.com>
2017-04-06 02:29:16 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, url_for, redirect
+import hashlib
+
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+
+from passlib.hash import sha256_crypt
+import datetime
+
 app = Flask(__name__)
+
+#configure postgreSQL
+try:
+    conn = psycopg2.connect(database="code",
+	    					user="bc1691",
+		    				password="StarszndFaults",
+			    			host="0.0.0.0",
+				    		port=9800)
+except:
+    print "Disaster"
+#cur = conn.cursor()
 
 @app.route("/")
 def hello():
