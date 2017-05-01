824a858d4b040fd0fb6e480ba8a6537f1c658b7b
iven-yang <risingsky1@gmail.com>
2017-05-01 17:22:48 -0400

    dank

--- __init__.py
+++ __init__.py
@@ -19,47 +19,47 @@ print "Connected\n"
 
 @app.route("/")
 def index():
-    cursor = conn.cursor()
-    query = 'SELECT * FROM petmodel ORDER BY pet_name ASC'
-    cursor.execute(query)
-    
-    data = cursor.fetchone()
-    if(data):
-        print data
-    cursor.close();
-    
-    return "IT'S WORKING I AM GETTING STRONGER."
+	cursor = conn.cursor()
+	query = 'SELECT * FROM petmodel ORDER BY pet_name ASC'
+	cursor.execute(query)
+	
+	data = cursor.fetchone()
+	if(data):
+		print data
+	cursor.close();
+	
+	return "IT'S WORKING I AM GETTING STRONGER."
 
 #Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
-    #grabs information from the forms
-    username = request.form['Username']
-    password = request.form['Password']
-    
-    #cursor used to send queries
-    cursor = conn.cursor()
-    #executes query
-    query = 'SELECT password FROM UserAccount WHERE username = %s'
-    cursor.execute(query, (username,))
-    
-    #stores the results in a variable
-    data = cursor.fetchone()
-    if(not data):
+	#grabs information from the forms
+	username = request.form['Username']
+	password = request.form['Password']
+	
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT password FROM UserAccount WHERE username = %s'
+	cursor.execute(query, (username,))
+	
+	#stores the results in a variable
+	data = cursor.fetchone()
+	if(not data):
 		error = 'Invalid login or username'
 		print("invalid username: %s", username)
 		return error
 	
-    cursor.close()
-    error = None
+	cursor.close()
+	error = None
 
-    if(sha256_crypt.verify(password, data[0])):
+	if(sha256_crypt.verify(password, data[0])):
 		#creates a session for the the user
 		#session is a built in
 		session['username'] = username
 		print("Login Success: %s", username)
 		return redirect(url_for('index'))
-    else:
+	else:
 		#returns an error message to the html page
 		error = 'Invalid login or username'
 		print("invalid login: %s", username)
@@ -68,35 +68,35 @@ def loginAuth():
 #Authenticates the register
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
-    #grabs information from the forms
-    username = request.form['Username']
-    password = request.form['Password']
-    email = request.form['email']
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	#grabs information from the forms
+	username = request.form['Username']
+	password = request.form['Password']
+	email = request.form['email']
+	timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 	
-    password2 = sha256_crypt.encrypt(password)
-    #cursor used to send queries
-    cursor = conn.cursor()
-    #executes query
-    query = 'SELECT * FROM UserAccount WHERE username = %s'
-    cursor.execute(query, (username,))
-    
-    #stores the results in a variable
-    data = cursor.fetchone()
-    #use fetchall() if you are expecting more than 1 data row
-    error = None
-    if(data):
+	password2 = sha256_crypt.encrypt(password)
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM UserAccount WHERE username = %s'
+	cursor.execute(query, (username,))
+	
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	error = None
+	if(data):
 		#If the previous query returns data, then user exists
 		error = "This user already exists"
 		print("This username exists already: %s", username)
 		return error
-    else:
-        ins = 'INSERT INTO UserAccount VALUES(%s, %s, %s, %s, %s)'
-        cursor.execute(ins, (username, password2, email, timestamp, timestamp))
-        conn.commit()
-        cursor.close()
+	else:
+		ins = 'INSERT INTO UserAccount VALUES(%s, %s, %s, %s, %s)'
+		cursor.execute(ins, (username, password2, email, timestamp, timestamp))
+		conn.commit()
+		cursor.close()
 		print("Reg Success: %s", username)
-        return redirect(url_for('index'))
+		return redirect(url_for('index'))
 
 #send user's longitude and latitude data
 @app.route('/sendLoc', methods=['GET', 'POST'])
@@ -148,7 +148,7 @@ def getNearbyUsers():
 		username = session['username']
 		query = 'SELECT * FROM Location WHERE username = %s'
 		cursor.execute(query, (username,))
-    
+	
 		#stores the results in a variable
 		data = cursor.fetchone()
 		latitude = data[1]
@@ -217,7 +217,7 @@ def viewPets():
 		username = session['username']
 		query = 'SELECT * FROM HavePet WHERE username = %s'
 		cursor.execute(query, (username,))
-    
+	
 		#stores the results in a variable
 		data = cursor.fetchall()
 		
@@ -247,4 +247,4 @@ def logout():
 
 app.secret_key = 'ihaveasecret'
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=9800)
+	app.run(host='0.0.0.0',port=9800)
